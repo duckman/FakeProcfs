bf1ea7a0c423417bb17c1c59a116c6734321a451
Lucas Hereld <duckman@piratehook.com>
2011-03-24 13:43:26 -0700

    Initial Commit

---
+++ src/fakeprocfs/FakeProcfs.java
@@ -0,0 +1,16 @@
+package fakeprocfs;
+
+/**
+ *
+ * @author duckman
+ */
+public class FakeProcfs
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
